c3df73f107d731043e1df9fb0e3caf9914d024f3
jonasglimm <jonas.glimmann@rwth-aachen.de>
2020-05-28 15:21:58 +0200

    Tried to integrate a settings panel

---
+++ Assets/MeineDaten/Scripts/Settings/SettingsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour
+{
+    private bool timerChecked;
+
+    private bool toucscreenChecked;
+    private bool touchpadChecked;
+    private bool iDriveChecked;
+    private bool gestenChecked;
+
+    private int taktLimit;
+    private int timer;
+
+    public GameObject aufgabenAnzahl;
+    public GameObject timerzeit;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
